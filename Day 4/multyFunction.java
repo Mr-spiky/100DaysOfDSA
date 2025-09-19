8eb8bfd170840ca320d97e1ab266935c923143ec
Spiky <shivsim2005@gmail.com>
2025-09-20 00:54:16 +0530

    day 4 update

---
+++ Day 4/multyFunction.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+public class multyFunction {
+
+    public static int calculateProduct(int a , int b){
+        int mult = a * b;
+        return mult;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        
+        System.out.println("Prod of two number is : " + calculateProduct(a, b));
+    }
+{
+
+}}
